fedce572e8b8d626d969349094858128b626a053
juanhts04 <juancarlossintor@gmail.com>
2025-02-02 20:18:54 -0500

    1454

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,17 +1,18 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { CourseFormComponent } from "../course-form/course-form.component";
 import { CourseListComponent } from "../course-list/course-list.component";
-import { TeacherFormComponent } from "../teacher-form/teacher-form.component";
+import { NavbarComponent } from "../navbar/navbar.component";
+import { SidebarComponent } from "../sidebar/sidebar.component";
 import { StudentFormComponent } from "../student-form/student-form.component";
 import { StudentListComponent } from "../student-list/student-list.component";
-import { SidebarComponent } from "../sidebar/sidebar.component";
-import { NavbarComponent } from "../navbar/navbar.component";
+import { TeacherFormComponent } from "../teacher-form/teacher-form.component";
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css'],
-  imports: [CourseFormComponent, CourseListComponent, TeacherFormComponent, StudentFormComponent, StudentListComponent, SidebarComponent, NavbarComponent]
+  imports: [CourseFormComponent, CourseListComponent, TeacherFormComponent, StudentFormComponent, StudentListComponent, SidebarComponent, NavbarComponent, CommonModule],
 })
 export class DashboardComponent {
   selectedContent: string = 'welcome';
